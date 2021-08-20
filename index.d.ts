7cb64f3d7644bd559ee8e78d6f40d5b7334f336f
Beatso <39217836+Beatso@users.noreply.github.com>
2021-08-20 20:50:29 +0100

    update declarations

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 export = scaleImage;
 /**
  * Resizes an image with nearest neighbour interpolation.
- * @param {buffer | Uint8Array | Uint8ClampedArray | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array | string} input - The image to resize.
+ * @param {Buffer | Uint8Array | Uint8ClampedArray | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array | string} input - The image to resize.
  * @param {number} scale - How many times bigger the image should be. Can be negative to shrink the image.
- * @returns {buffer} - The resized image.
+ * @returns {Buffer} - The resized image.
 */
-declare function scaleImage(input: any | Uint8Array | Uint8ClampedArray | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array | string, scale: number): any;
+declare function scaleImage(input: Buffer | Uint8Array | Uint8ClampedArray | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array | string, scale: number): Buffer;
